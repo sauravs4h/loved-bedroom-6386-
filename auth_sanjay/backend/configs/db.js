8b9fd192355170d5f78a13400132926e1a109d0b
Saurav Sharma <112651044+sauravs4h@users.noreply.github.com>
2023-02-27 00:03:40 +0530

    Delete auth_sanjay directory

--- auth_sanjay/backend/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect("mongodb+srv://sanjayv:sanjay@cluster0.9ycsbw8.mongodb.net/nxm201cw?retryWrites=true&w=majority");
-
-module.exports = { connection }
